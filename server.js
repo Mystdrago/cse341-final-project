c9b7d412cb3ed11b9efd703133c6f5f035fee4e1
Curtis Halls <curtishalls@ymail.com>
2025-11-28 17:04:53 -0700

    sigh

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
-const app =express();
+const app = express();
 
 const port = process.env.PORT || 3002;
 
